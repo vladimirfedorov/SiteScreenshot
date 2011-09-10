af21764439742f35622d504e8b68f2de36a1187c
Vladimir Fedorov <fedorovvs@gmail.com>
2011-09-11 01:58:03 +0600

    1st commit- ready-to-use app

---
+++ SiteScreenshot/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using sitethmb;
+using System.Reflection;
+using System.IO;
+
+namespace SiteScreenshot
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // SiteScreenshot http://www.sitename.com/page.html 1280 1024 ./image.jpg
+            // SiteScreenshot http://www.sitename.com/page.html 1280 ./image.jpg
+
+            if (args.Length == 3)
+                CreateSiteScreenshot(args[0], args[1], "1", false, args[2]);
+            else if (args.Length == 4)
+                CreateSiteScreenshot(args[0], args[1], args[2], true, args[3]);
+            else
+                ShowInfo();
+        
+        }
+
+        static void ShowInfo()
+        {
+            Console.WriteLine("Params format: ");
+            Console.WriteLine("  SiteScreenshot URL Width [HeightLimit] OutputFileName");
+            Console.WriteLine("Examples:");
+            Console.WriteLine("  SiteScreenshot http://www.sitename.com/page.html 1280 1024 ./image.jpg");
+            Console.WriteLine("  SiteScreenshot http://www.sitename.com/page.html 1280 ./image.jpg");
+        }
+
+        static void CreateSiteScreenshot(string Url, string sWidth, string sHeight, bool Limit, string FileName)
+        {
+            int Width=0, Height=0;
+            if (!int.TryParse(sWidth, out Width)) { Console.WriteLine("Wrong width: " + sWidth); return; }
+            if (!int.TryParse(sHeight, out Height)) { Console.WriteLine("Wrong height: " + sHeight); return; }
+            
+            string outfilename = 
+                Environment.ExpandEnvironmentVariables(
+                    Path.GetFullPath(FileName)); ;
+
+
+            SiteThmb thmb = new SiteThmb(Url, outfilename, Width, Height);
+            thmb.LimitHeight = Limit;
+            thmb.SaveSiteImage();
+
+        }
+    }
+}
---
+++ sitethmb/SiteThmb.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace sitethmb
+{
+    
+    public class SiteThmb
+    {
+        /// <summary>
+        /// Internet address of a site
+        /// </summary>
+        public string Url;
+
+        public string FileName;
+        
+        /// <summary>
+        /// Size of the site image
+        /// </summary>
+        public Size SiteImageSize;
+
+        public bool LimitHeight = true;
+
+        public Image SiteImage;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public SiteThmb()
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="Url">Internet address of a site</param>
+        public SiteThmb(string Url, string FileName, int Width, int Height)
+        {
+            this.Url = Url;
+            this.FileName = FileName;
+            this.SiteImageSize = new Size(Width, Height);
+        }
+
+        private void wb_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            WebBrowser browser = (WebBrowser)sender;
+            if (browser.ReadyState != WebBrowserReadyState.Complete) return;
+
+            if (!LimitHeight) browser.Height = browser.Document.Body.ScrollRectangle.Height;
+            using (Graphics graphics = browser.CreateGraphics())
+            using (Bitmap bitmap = new Bitmap(
+                browser.Width, 
+                LimitHeight ? browser.Height : browser.Document.Body.ScrollRectangle.Height, 
+                graphics))
+            //using (Bitmap bitmap = new Bitmap(browser.Width, browser.Height, graphics))
+                {
+                    Rectangle bounds = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+                    browser.DrawToBitmap(bitmap, bounds);
+                    this.SiteImage = bitmap;
+                    bitmap.Save(this.FileName, ImageFormat.Jpeg);
+               }
+        }
+
+        /// <summary>
+        /// Save image of a site
+        /// </summary>
+        /// <param name="FileName">File name to save to</param>
+        public void SaveSiteImage()
+        {
+            SaveSiteImage(this.Url, this.FileName);
+        }
+        
+        /// <summary>
+        /// Save image of a site
+        /// </summary>
+        /// <param name="Url">Internet address of a site</param>
+        /// <param name="FileName">File name to save to</param>
+        public void SaveSiteImage(string Url, string FileName)
+        {
+            this.Url = Url;
+            this.FileName = FileName;
+
+            WebBrowser wb = new WebBrowser();
+            wb.Size = this.SiteImageSize;
+            wb.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(wb_DocumentCompleted);
+            wb.ScrollBarsEnabled = true;
+
+            wb.Navigate(Url);
+            while (wb.ReadyState != WebBrowserReadyState.Complete)
+                Application.DoEvents();
+
+        }
+
+        
+    }
+}
